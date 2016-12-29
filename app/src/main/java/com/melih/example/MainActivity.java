4a46d80d10fe57eb5c1b98d315c183f0fd8423c5
melih <aksoy.melihcan@gmail.com>
2016-12-29 10:38:28 +0300

    Updated example

--- app/src/main/java/com/melih/example/MainActivity.java
+++ app/src/main/java/com/melih/example/MainActivity.java
@@ -9,46 +9,53 @@ import com.mobven.example.R;
 
 public class MainActivity extends AppCompatActivity {
 
+	private HoldToLoadLayout mHoldToLoadLayout;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		final HoldToLoadLayout holdToLoadLayout = (HoldToLoadLayout) findViewById(R.id.holdToLoadLayout);
+		mHoldToLoadLayout = (HoldToLoadLayout) findViewById(R.id.holdToLoadLayout);
 
-		if (holdToLoadLayout != null) {
-			holdToLoadLayout.setStrokeWidth(10);
-			holdToLoadLayout.setStrokeAlpha(255);
-			holdToLoadLayout.setPlayReverseAnimation(true);
-			holdToLoadLayout.setStopWhenFilled(false);
-			holdToLoadLayout.setColorAnimator(Color.YELLOW, Color.RED);
-			holdToLoadLayout.setStartAngle(0);
+		mHoldToLoadLayout.setStrokeWidth(10);
+		mHoldToLoadLayout.setStrokeAlpha(255);
+		mHoldToLoadLayout.setPlayReverseAnimation(true);
+		mHoldToLoadLayout.setStopWhenFilled(false);
+		mHoldToLoadLayout.setColorAnimator(Color.YELLOW, Color.RED);
+		mHoldToLoadLayout.setStartAngle(HoldToLoadLayout.Angle.BOTTOM);
 
-			/**
-			 * Enabling this will suppress the use of stopWhenFilled and cause progress to stop at all times
-			 */
-			holdToLoadLayout.setHoldAtLastPosition(true);
+		/**
+		 * Enabling this will suppress the use of stopWhenFilled and cause progress to stop at all times
+		 */
+		mHoldToLoadLayout.setHoldAtLastPosition(true);
 
-			holdToLoadLayout.setFillListener(new HoldToLoadLayout.FillListener() {
-				@Override
-				public void onFull() {
+		mHoldToLoadLayout.setFillListener(new HoldToLoadLayout.FillListener() {
+			@Override
+			public void onFull() {
 
-				}
+			}
 
-				@Override
-				public void onEmpty() {
-				}
+			@Override
+			public void onEmpty() {
 
-				@Override
-				public void onAngleChanged(float angle) {
+			}
 
-				}
+			@Override
+			public void onAngleChanged(float angle) {
 
-				@Override
-				public void onOffsetChanged(float offset) {
+			}
 
-				}
-			});
-		}
+			@Override
+			public void onOffsetChanged(float offset) {
+
+			}
+		});
+	}
+
+	@Override
+	protected void onDestroy() {
+		mHoldToLoadLayout.removeFillListener();
+		super.onDestroy();
 	}
 }
